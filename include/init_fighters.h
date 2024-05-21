7cb3574e47a78f643a3fbe1f760caa944170c360
SIMLUKE <luc.simon@epitech.eu>
2024-05-21 20:14:19 +0200

    fix: sprites are normalized

--- include/init_fighters.h
+++ include/init_fighters.h
@@ -25,7 +25,7 @@ static const movement_init_t MOVES_INIT[] = {
         IDLE,
         "idle.png",
         200,
-        {0, 0, 52, 100},
+        {0, 0, 60, 100},
         3
     },
     {
@@ -46,56 +46,56 @@ static const movement_init_t MOVES_INIT[] = {
         CROUCH,
         "crouch.png",
         150,
-        {0, 0, 50, 100},
+        {0, 0, 55, 100},
         2
     },
     {
         JUMP,
         "jump.png",
         125,
-        {0, 0, 50, 100},
+        {0, 0, 70, 100},
         6
     },
     {
         ATTACK,
         "attack.png",
         100,
-        {0, 0, 75, 100},
+        {0, 0, 80, 100},
         3
     },
     {
         ATTACK_UP,
         "attack_up.png",
         150,
-        {0, 0, 70, 130},
+        {0, 0, 80, 130},
         3
     },
     {
         ATTACK_DOWN,
         "attack_down.png",
         100,
-        {0, 0, 67, 100},
+        {0, 0, 80, 100},
         3
     },
     {
         ATTACK_CROUCH,
         "attack_crouch.png",
         100,
-        {0, 0, 75, 110},
+        {0, 0, 80, 110},
         3
     },
     {
         ATTACK_JUMP,
         "attack_jump.png",
         100,
-        {0, 0, 75, 100},
+        {0, 0, 80, 100},
         3
     },
     {
         HIT,
         "hit.png",
         100,
-        {0, 0, 52, 112},
+        {0, 0, 55, 112},
         1
     },
     {
@@ -117,13 +117,13 @@ typedef struct fighter_init_s {
 
 static const fighter_init_t FIGHTER_INIT[] = {
     [PLAYER_FIGHTER] = {
-        "assets/fighters/player/",
+        "assets/fighters/black_guy/",
         PLAYER_IDLE_TEXT,
         PLAYER_TEXTURES_END,
         300
     },
     [NPC1] = {
-        "assets/fighters/npc1/",
+        "assets/fighters/black_guy/",
         NPC_IDLE_TEXT,
         NPC_TEXTURES_END,
         300,
